474846c8e5923ec1f8bbcc91d2cd9f9f08a95e06
hrparkdev <hrparkdev@gmail.com>
2024-02-18 09:31:48 +0900

    Chapter04

---
+++ Chapter04/OctHex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 7;
+	int num2 = 13;
+	printf("%o %#o \n", num1, num1); //07
+	printf("%x %#x \n", num2, num2); //0xd
+	return 0;
+}
---
+++ Chapter04/ScanConvTwo.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	float num1;
+	double num2;
+	long double num3;
+
+	printf("실수 입력1(e 표기법으로): ");
+	scanf("%f", &num1);
+	printf("입력된 실수 %f \n", num1);
+
+	printf("실수 입력2(e 표기법으로): ");
+	scanf("%lf", &num2);
+	printf("입력된 실수 %f \n", num1);
+
+	printf("실수 입력3(e 표기법으로): ");
+	scanf("%Lf", &num3);
+	printf("입력된 실수 %Lf \n", num1);
+
+	return 0;
+}
